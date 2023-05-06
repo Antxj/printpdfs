0c1fa7e13f0a9bb46af64de809b10ff5d9105717
Antxj <antonioxaraj@gmail.com>
2023-05-06 08:03:43 -0300

    v1.2

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
+
 import PyPDF2
+from PyPDF2 import PdfFileMerger
+from pypdf import PdfMerger
+
 import win32api
 from os import system
 
 system("title " + "Antonio Xara")
 
-directory = os.getcwd()
-folder_path = rf"{directory}"
+folder_path = os.getcwd()
+folder_path_saida = os.getcwd() + r'\resultado'
+
+pdfs_to_merge = []
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".pdf"):
 
-        # Abre o arquivo PDF
         filepath = os.path.join(folder_path, filename)
         with open(filepath, "rb") as f:
 
@@ -26,11 +31,20 @@ for filename in os.listdir(folder_path):
                 pdf_writer = PyPDF2.PdfWriter()
                 pdf_writer.add_page(first_page)
                 pdf_writer.write(out_f)
-                print(f'Arquivo enviado para impressão: {output_filename}')
-                win32api.ShellExecute(0, "print", output_filepath, None, ".", 0)
+
+        pdfs_to_merge.append(output_filepath)
+        merger = PdfMerger()
+        for pdf in pdfs_to_merge:
+            merger.append(pdf)
+        merger.write("1-RESULTADO.pdf")
+        merger.close()
 
 
-print('Pronto!')
+                # print(f'Arquivo enviado para impressão: {output_filename}')
+                # win32api.ShellExecute(0, "print", output_filepath, None, ".", 0)
+
+
+print('Pronto, o arquivo compilado foi criado!')
 
 confirmation = input('Pressione qualquer tecla para encerrar...')
 quit()
