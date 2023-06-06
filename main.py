12b1213caa5137648e841d3a1fec65d5a923e0d3
Antxj <antonioxaraj@gmail.com>
2023-06-06 10:15:32 -0300

    v1.2

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import os
-import time
-
 import PyPDF2
-from pypdf import PdfMerger
-from os import system
-
-system("title " + "Antonio Xara" + "v1.2")
+from PyPDF2 import PdfMerger
 
 folder_path = os.getcwd()
 folder_path_saida = os.getcwd() + r'\resultado'
@@ -15,41 +10,34 @@ count = 0
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".pdf"):
-        count = count + 1
-
+        count += 1
         filepath = os.path.join(folder_path, filename)
         with open(filepath, "rb") as f:
-
             pdf_reader = PyPDF2.PdfReader(f)
-
             first_page = pdf_reader.pages[0]
-
             output_filename = "pag1_" + os.path.splitext(filename)[0] + ".pdf"
             output_filepath = os.path.join(folder_path, output_filename)
             print(f'Processando o arquivo: {filename}')
-
             with open(output_filepath, "wb") as out_f:
                 pdf_writer = PyPDF2.PdfWriter()
                 pdf_writer.add_page(first_page)
                 pdf_writer.write(out_f)
-
         pdfs_to_merge.append(output_filepath)
-        merger = PdfMerger()
-        for pdf in pdfs_to_merge:
-            merger.append(pdf)
-        merger.write("1-RESULTADO.pdf")
-        merger.close()
 
+merger = PdfMerger()
+for pdf in pdfs_to_merge:
+    merger.append(pdf)
 
-for qq in pdfs_to_merge:
-    os.remove(f'{qq}')
+merger.write("1-RESULTADO.pdf")
+merger.close()
+
+for file_path in pdfs_to_merge:
+    os.remove(file_path)
 
 print(f'Arquivos processados com sucesso: {count}')
 
-                # print(f'Arquivo enviado para impressão: {output_filename}')
-                # win32api.ShellExecute(0, "print", output_filepath, None, ".", 0)
+# Tela para fechar o programa
+confirmation = input('Pressione qualquer tecla para encerrar...')
+quit()
 
-# confirmation = input('Pressione qualquer tecla para encerrar...')
-# quit()
-
-# pyinstaller --onefile --clean --name PDF Print 1-Page main.py
+# pyinstaller --onefile --clean --name PDF-Print-1-Page main.py
